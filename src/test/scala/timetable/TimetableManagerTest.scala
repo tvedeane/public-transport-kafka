dba93f2643220a4dba92d9907260c4ff88264188
Marek Jeszka <1138746+marekjeszka@users.noreply.github.com>
2020-07-06 20:41:31 +0200

    Add missing mocks

--- src/test/scala/timetable/TimetableManagerTest.scala
+++ src/test/scala/timetable/TimetableManagerTest.scala
@@ -2,12 +2,16 @@ package timetable
 
 import java.time.LocalTime
 
+import delays.DelayManager
 import org.scalatest.funsuite.AnyFunSuite
+import org.mockito.Mockito._
 
 class TimetableManagerTest extends AnyFunSuite {
 
   test("finds next line at stop") {
+    val delayManagerMock = mock(classOf[DelayManager])
     val timetableManager = new InMemoryTimetableManager(
+      delayManagerMock,
       Seq(Line(1, "A1")), Seq(Stop(1, 2, 2)), Seq(Time(1, 1, LocalTime.of(8, 10))))
 
     assert(timetableManager.getNextLine(1, LocalTime.of(8, 9)).contains("A1"))
@@ -15,7 +19,9 @@ class TimetableManagerTest extends AnyFunSuite {
   }
 
   test("finds line at location") {
+    val delayManagerMock = mock(classOf[DelayManager])
     val timetableManager = new InMemoryTimetableManager(
+      delayManagerMock,
       Seq(Line(1, "A1")), Seq(Stop(1, 2, 2)), Seq(Time(1, 1, LocalTime.of(8, 10))))
 
     assert(timetableManager.getLineAt(2, 2, LocalTime.of(8, 10)).contains("A1"))
